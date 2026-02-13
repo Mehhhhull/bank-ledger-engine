37c9fb05d96d6fee05b35ce4407cbaf0ed5c9ab8
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-02-13 19:10:06 +0530

    Routes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@
 const express=require('express');
 const cookieParser=require("cookie-parser")
 
-const authRouter=require("./routes/auth.routes")
 
 
 const app=express();
@@ -11,6 +10,18 @@ app.use(express.json()) //this will allow us to parse the json data sent by the 
 
 app.use(cookieParser()) //this will allow us to parse the cookies sent by the client in the request headers. we need this to get the token from the cookies during authentication. without this, we won't be able to access req.cookies and it will be undefined. this is a middleware that parses Cookie header and populates req.cookies with an object keyed by the cookie names. it also supports signed cookies, which are cookies that have been signed with a secret to prevent tampering.
 
+/**
+ * -- Routes required
+ */
+const authRouter=require("./routes/auth.routes")
+const accountRouter=require("./routes/account.routes")
+
+
+/**
+ * -- Use Routes
+ */
 app.use("/api/auth",authRouter)
+app.use("/api/accounts",accountRouter)
+
 
 module.exports=app
