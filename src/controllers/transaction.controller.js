62c5db0e1f7aa18f357e3752ab6ee361141179e0
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-02-19 20:45:33 +0530

    ..

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -178,7 +178,6 @@ async function createInitialFundsTransaction(req, res) {
   }
 
   const fromUserAccount=await accountModel.findOne({
-    systemUser:true,//....
     user:req.user._id
   })
 
@@ -192,27 +191,27 @@ async function createInitialFundsTransaction(req, res) {
   const session=await mongoose.startSession()
   session.startTransaction()
 
-  const transaction=await transactionModel.create({
+  const transaction=new transactionModel({
     fromAccount: fromUserAccount._id,
     toAccount,
     amount,
     idempotencyKey,
     status:"PENDING"
-  },{session})
+  })
 
-  const debitLedgerEntry=await ledgerModel.create({
+  const debitLedgerEntry=await ledgerModel.create([{
     account: fromUserAccount._id,
     amount:amount,
     transaction:transaction._id,
     type:"DEBIT"
-},{session})
+}],{session})
 
-const creditLedgerEntry=await ledgerModel.create({
+const creditLedgerEntry=await ledgerModel.create([{
     account: toAccount._id,
     amount:amount,
     transaction:transaction._id,
     type:"CREDIT"
-},{session})
+}],{session})
 
 transaction.status="COMPLETED"
 await transaction.save({session})
