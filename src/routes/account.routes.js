3c0cc8136953ae8897d66319a3c79feeaa9fa78a
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-02-19 21:15:24 +0530

    ..

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -12,4 +12,20 @@ const router=express.Router()
  */
 router.post("/",authMiddleware.authMiddleware,accountController.createAccountController)
 
+/**
+ * -GET /api/accounts/
+ * -Create all accounts of the logged-in user
+ * -Protected Route
+ */
+
+router.get("/",authMiddleware.authMiddleware,accountController.getUserAccountsController)
+
+/**
+ * -GET /api/accounts/balance/:accountId
+ * 
+ * 
+ */
+
+router.get("/balance/:accountId",authMiddleware.authMiddleware,accountController.getAccountBalanceController)
+
 module.exports=router
